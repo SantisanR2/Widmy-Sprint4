49037c0f3fb867fc55490858dbd7053abaac2b2f
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2023-05-29 11:45:53 -0500

    Correccion

--- personalSalud/personalSalud/urls.py
+++ personalSalud/personalSalud/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^personalSalud$', views.personalSalud, name='personalSalud'),
+    url("", views.personalSalud, name='personalSalud'),
 ]
