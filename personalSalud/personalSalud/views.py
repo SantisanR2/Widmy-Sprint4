18c485c1fc0a890927f7dfaada28205b7eaed8ae
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2023-05-29 12:41:15 -0500

    Correccion

--- personalSalud/personalSalud/views.py
+++ personalSalud/personalSalud/views.py
@@ -4,7 +4,6 @@ from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 import requests
-import json as j
 
 @api_view(["GET", "POST"])
 def personalSalud(request):
@@ -28,10 +27,12 @@ def personalSalud(request):
         data = JSONParser().parse(request)
 
         r = requests.post(settings.PATH_ENCRYPT, data={'string_to_hash': data['nombre']})
-        hash = r['hash']
+        hash = r.json()
+        hash = hash['hash']
 
         s = requests.post(settings.PATH_ENCRYPT, data={'string_to_hash': data['nombre']})
-        hash2 = r['hash']
+        hash2 = r.json()
+        hash2 = hash2['hash']
 
         if(hash == hash2):
             result = personalSalud.insert_one(data)
