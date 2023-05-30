b3fbd5de2a8b289e2f7d210a1815962a8f19683f
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2023-05-30 00:15:17 -0500

    Correcion

--- finanzas/finanzas/views.py
+++ finanzas/finanzas/views.py
@@ -5,7 +5,7 @@ from .models import factura
 
 def listar_facturas(request):
     queryset = factura.objects.all()
-    queryset = queryset.filter(fecha__gte=date.today()-timedelta(days=180))
+    queryset = queryset.filter(fecha__gte=date.today()-timedelta(days=180)).order_by('fecha')
     context = list(queryset.values('fecha', 'ciudad', 'servicio', 'facturacion'))
     return JsonResponse(context, safe=False)
 
